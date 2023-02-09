0100185aa2be1ef8060047074c5247d90d4a26a8
Joseph Assiga <jassigaa@redhat.com>
2023-02-09 19:46:48 +0100

    Adding Expnse Restfull Service

---
+++ quarkus-expense-restful-service/src/test/java/quarkus/online/summit/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package quarkus.online.summit;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
