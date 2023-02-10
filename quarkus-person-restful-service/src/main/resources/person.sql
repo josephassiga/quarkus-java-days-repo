e6ac45a19681ead48c537c2a5cf6652ad8d7db7b
Joseph Assiga <jassigaa@redhat.com>
2023-02-10 14:22:56 +0100

    Add new sql entries

--- quarkus-person-restful-service/src/main/resources/person.sql
+++ quarkus-person-restful-service/src/main/resources/person.sql
@@ -1,5 +1,6 @@
 INSERT INTO person(id, name, birth, eyes) VALUES (nextval('hibernate_sequence'), 'Stephan Kraft', '1976-12-22', 'BLUE');
 INSERT INTO person(id, name, birth, eyes) VALUES (nextval('hibernate_sequence'), 'Salvador L. Witcher', '1984-05-24', 'BROWN');
 INSERT INTO person(id, name, birth, eyes) VALUES (nextval('hibernate_sequence'), 'Kim Hu', '1999-04-25', 'HAZEL');
-INSERT INTO person(id, name, birth, eyes) VALUES (nextval('hibernate_sequence'), 'Java Dev23', '2023-02-21', 'BLACK');
+INSERT INTO person(id, name, birth, eyes) VALUES (nextval('hibernate_sequence'), 'Anakim Skywalker', '1950-02-21', 'BLACK');
+INSERT INTO person(id, name, birth, eyes) VALUES (nextval('hibernate_sequence'), 'Joseph ASSIGA', '1986-02-21', 'BLACK');
 
